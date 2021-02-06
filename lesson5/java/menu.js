aa66e9cf2b4c417e226bbebedf21ed89173ee9e0
Use your Github username <Enter your email address>
2021-02-06 14:06:41 -0700

    initial commit

--- lesson5/java/menu.js
+++ lesson5/java/menu.js
@@ -15,7 +15,7 @@ document.getElementById("currentDate").innerHTML = dateStr;
 document.getElementById("currentYear").innerHTML = year;
 
 if (day1[day] == 'Friday') {
-    document.getElementById("friday").innerHTML = "Friday = Preston Pancakes in the Park!  9:00 a.m. Friday at the city park pavilion.";
+    document.getElementById("friday").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
     document.getElementById("friday").style.display = "block";
   }
   else {
