f045fdf835b01719a9c3a861b03d53f9e1a4168d
Use your Github username <Enter your email address>
2021-02-19 11:39:41 -0700

    initial commit

--- lesson5/java/menu.js
+++ lesson5/java/menu.js
@@ -5,7 +5,7 @@ function toggleMenu() {
 const day1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const month1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 var d = new Date();
-var day = d.getDay() 
+var day = d.getDay(); 
 var date = d.getDate();
 var month = d.getMonth();
 var year = d.getFullYear();
