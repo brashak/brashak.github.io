7dd9f0d103c8e3034389dfac1ca00f902d51e2fe
Use your Github username <Enter your email address>
2021-03-10 15:59:32 -0700

    initial commit

---
+++ lesson10/java/lesson10.js
@@ -0,0 +1,50 @@
+//hamburger menu
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("menu-id");
+  }
+  //footer date
+  const day1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const month1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var d = new Date();
+  var day = d.getDay(); 
+  var date = d.getDate();
+  var month = d.getMonth();
+  var year = d.getFullYear();
+  var dateStr = day1[day] + ', ' + date + " " + month1[month] + " " + year;
+  document.getElementById("currentDate").innerHTML = dateStr;
+  document.getElementById("currentYear").innerHTML = year;
+  //pancakes
+  if (day1[day] == 'Friday') {
+      document.getElementById("friday").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+      document.getElementById("friday").style.display = "block";
+    }
+    else {
+      document.getElementById("friday").style.display = "none";
+    }
+  
+    function adjustSeverity(rating) {
+      document.getElementById("ratingvalue").innerHTML = rating;
+  } 
+  //forecast
+  const apiURL_forecast = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=da923bb8ec61575a7dfe5e3106bb43c1"
+
+  fetch(apiURL_forecast)
+      .then(response => response.json())
+      .then((jsObject) => {
+          console.log(jsObject);
+          const forecastData = jsObject.list.filter((element)=>element.dt_txt.includes('18:00:00'));
+  
+      console.log(forecastData);
+  
+      const weekdays = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+  
+      let day = 0;
+      forecastData.forEach(forecast => {
+        let x = new Date(forecast.dt_txt);
+      document.getElementById('temp'+(day+1)).textContent = Math.round(forecast.main.temp) + ' °F';
+      document.getElementById('img'+(day+1)).src = "https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
+      document.getElementById('img'+(day+1)).alt = forecast.weather[0].description
+      document.getElementById('day'+(day+1)).textContent = weekdays[x.getDay()];
+      day++;	  
+      });
+  }); 
