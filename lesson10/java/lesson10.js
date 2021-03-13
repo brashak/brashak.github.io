f8e47d045a41c68c32a80aee9b0ad004f6161661
Use your Github username <Enter your email address>
2021-03-13 10:56:06 -0700

    initial commit

--- lesson10/java/lesson10.js
+++ lesson10/java/lesson10.js
@@ -1,7 +1,7 @@
 //hamburger menu
 function toggleMenu() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("menu-id");
-  }
+    document.getElementsByClassName("navigation")[0].classList.toggle("menu-id"); }
+
   //footer date
   const day1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const month1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
