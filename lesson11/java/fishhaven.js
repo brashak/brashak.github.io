420c97636ad75e73bdf78c9fe27e1f483b7247fb
Use your Github username <Enter your email address>
2021-03-15 18:28:21 -0600

    initial commit

--- lesson11/java/fishhaven.js
+++ lesson11/java/fishhaven.js
@@ -26,38 +26,40 @@ function toggleMenu() {
       document.getElementById("ratingvalue").innerHTML = rating;
   } 
   //summary
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5585010&units=imperial&APPID=da923bb8ec61575a7dfe5e3106bb43c1";
+  const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5585010&units=imperial&APPID=da923bb8ec61575a7dfe5e3106bb43c1";
 
-fetch(apiURL)
- .then((response) => response.json())
- .then((town) => {
-   console.log(town);
-   let description = town.weather[0].description;
-   document.getElementById('currently').innerHTML = description.charAt(0).toUpperCase() + description.slice(1);
-   document.getElementById('temp').innerHTML = Math.round(town.main.temp);
-   document.getElementById('humidity').innerHTML = town.main.humidity;
-   document.getElementById('windspeed').innerHTML = Math.round(town.wind.speed);
-});
+  fetch(apiURL)
+  .then((response) => response.json())
+  .then((town) => {
+    console.log(town);
+    let description = town.weather[0].description;
+    document.getElementById('currently').innerHTML = description.charAt(0).toUpperCase() + description.slice(1);
+    document.getElementById('temp').innerHTML = Math.round(town.main.temp);
+    document.getElementById('humidity').innerHTML = town.main.humidity;
+    document.getElementById('windspeed').innerHTML = Math.round(town.wind.speed);
+  });
+  
   //forecast
+  
   const apiURL_forecast = "https://api.openweathermap.org/data/2.5/forecast?id=5585010&units=imperial&APPID=da923bb8ec61575a7dfe5e3106bb43c1"
-
+  
   fetch(apiURL_forecast)
-      .then(response => response.json())
-      .then((jsObject) => {
-          console.log(jsObject);
-          const forecastData = jsObject.list.filter((element)=>element.dt_txt.includes('18:00:00'));
+    .then(response => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        const forecastData = jsObject.list.filter((element)=>element.dt_txt.includes('18:00:00'));
   
-      console.log(forecastData);
+  console.log(forecastData);
   
-      const weekdays = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+  const weekdays = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
   
-      let day = 0;
-      forecastData.forEach(forecast => {
-        let x = new Date(forecast.dt_txt);
-      document.getElementById('temp'+(day+1)).textContent = Math.round(forecast.main.temp) + ' °F';
-      document.getElementById('img'+(day+1)).src = "https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
-      document.getElementById('img'+(day+1)).alt = forecast.weather[0].description
-      document.getElementById('day'+(day+1)).textContent = weekdays[x.getDay()];
-      day++;	  
-      });
-  }); 
+    let day = 0;
+  forecastData.forEach(forecast => {
+    let x = new Date(forecast.dt_txt);
+    document.getElementById('temp'+(day+1)).textContent = Math.round(forecast.main.temp) + ' °F';
+    document.getElementById('img'+(day+1)).src = "https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
+    document.getElementById('img'+(day+1)).alt = forecast.weather[0].description
+  document.getElementById('day'+(day+1)).textContent = weekdays[x.getDay()];
+  day++;	  
+  });
+  });
