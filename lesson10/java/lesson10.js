df66c8475c9c7d0621e5f61a1cba03dc425b5774
Use your Github username <Enter your email address>
2021-03-19 15:44:53 -0600

    initial commit

--- lesson10/java/lesson10.js
+++ lesson10/java/lesson10.js
@@ -39,7 +39,7 @@ fetch(apiURL)
    document.getElementById('windspeed').innerHTML = Math.round(town.wind.speed);
 });
   //forecast
-  const apiURL_forecast = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=da923bb8ec61575a7dfe5e3106bb43c1"
+  const apiURL_forecast = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=da923bb8ec61575a7dfe5e3106bb43c1";
 
   fetch(apiURL_forecast)
       .then(response => response.json())
@@ -56,13 +56,13 @@ fetch(apiURL)
         let x = new Date(forecast.dt_txt);
       document.getElementById('temp'+(day+1)).textContent = Math.round(forecast.main.temp) + ' °F';
       document.getElementById('img'+(day+1)).src = "https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
-      document.getElementById('img'+(day+1)).alt = forecast.weather[0].description
+      document.getElementById('img'+(day+1)).alt = forecast.weather[0].description;
       document.getElementById('day'+(day+1)).textContent = weekdays[x.getDay()];
       day++;	  
       });
    
   //windchill
-  let output = "N/A"
+  let output = "N/A";
   if (temp <= 50 && windspeed > 3) {
       output = Math.round(35.74 + 0.6215 * temp - 35.75 * windspeed ** 0.16 + 0.4275 * temp * windspeed ** 0.16);
       output += " \xB0F";
