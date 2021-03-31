7bed40a892ddb95d3d9edb3a975fddd3e0752aae
Use your Github username <Enter your email address>
2021-03-31 09:08:21 -0600

    initial commit

--- final/js/directory.js
+++ final/js/directory.js
@@ -1,1 +1,18 @@
-const requestURL = "/final/json/directory.json";
+const requestURL = 'https://brashak.github.io/directory/directory.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);
+     const directory = jsonObject['directory'];
+      for (let i = 0; i < directory.length; i++) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        h2.textContent = directory[i].businessname;
+        card.appendChild(h2);
+        
+        document.querySelector('div.cards').appendChild(card);
+});
+   
