dfba2995c7ff91be709daca95f182ebb676af885
Use your Github username <Enter your email address>
2021-03-31 09:57:59 -0600

    initial commit

--- final/js/directory.js
+++ final/js/directory.js
@@ -1,18 +1,35 @@
-const requestURL = 'https://brashak.github.io/directory/directory.json';
+const requestURL = 'https://brashak.github.io/directory.json';
 
 fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);
-     const directory = jsonObject['directory'];
-      for (let i = 0; i < directory.length; i++) {
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        h2.textContent = directory[i].businessname;
-        card.appendChild(h2);
-        
-        document.querySelector('div.cards').appendChild(card);
+.then(function (response) {
+   return response.json();
+})
+.then(function (jsonObject) {
+   console.table(jsonObject);  // temporary checking for valid response and data parsing
+   const directory = jsonObject['directory'];
+   for (let i = 0; i < directory.length; i++) {
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let image = document.createElement('img');
+      let  phonenumber = document.createElement('div');
+      let  email = document.createElement('div');
+      let website= document.createElement('div');
+      h2.textContent = directory[i].businessname;
+      phonenumber.setAttribute('class', 'phonenumber')
+      phonenumber.textContent = 'Phone Number: ' + directory[i].phonenumber;
+      email.setAttribute('class', 'email')
+      email.textContent = 'Email: ' + directory[i].email;
+      website.setAttribute('class', 'website')
+      website.textContent = 'Website: ' + directory[i].website;
+      image.setAttribute('src', directory[i].imageurl);
+      image.setAttribute('alt', directory[i].businessname);
+      card.appendChild(h2);
+      card.appendChild(phonenumber);
+      card.appendChild(email);
+      card.appendChild(website);
+      card.appendChild(image);
+      document.querySelector('div.cards').appendChild(card);
+   }
 });
+
    
