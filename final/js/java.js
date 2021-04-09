24bc2cd2030d17a490e811ab83ad67738c8a5395
Use your Github username <Enter your email address>
2021-04-08 20:29:01 -0600

    initial commit

--- final/js/java.js
+++ final/js/java.js
@@ -1,7 +1,6 @@
 //hamburger
 function toggleMenu() {
-  document.getElementsByClassName("navigation")[0].classList.toggle("menu-id");
-}
+  document.getElementsByClassName("navigation")[0].classList.toggle("menu-id"); }
 
 //footer date
 const day1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
