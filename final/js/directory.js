3ad4230d5be77f2a1020085b5d0cad94a59b35bd
Use your Github username <Enter your email address>
2021-04-08 18:16:41 -0600

    initial commit

--- final/js/directory.js
+++ final/js/directory.js
@@ -32,25 +32,6 @@ fetch(requestURL)
    }
 });
 
-// Get the elements with class="column"
-var elements = document.getElementsByClassName(".cards");
-
-// Declare a loop variable
-var i;
-
-// List View
-function gridView() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.width = "100%";
-  }
-}
-
-// Grid View
-function listView() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.width = "50%";
-  }
-}
 
 
 
