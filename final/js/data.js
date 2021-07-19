c67f0ee8be5d4ceec341f21ed79b9bd997b375e7
Use your Github username <Enter your email address>
2021-07-19 09:23:49 -0600

    initial commit

---
+++ final/js/data.js
@@ -0,0 +1,41 @@
+const requestURL = 'https://brashak.github.io/final/data/data.json';
+
+fetch(requestURL)
+.then(function (response) {
+   return response.json();
+})
+.then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['data'];
+    for (let i = 0; i < data.length; i++) {
+       let card = document.createElement('section');
+       let h2 = document.createElement('h2');
+       let image = document.createElement('img');
+       let  maxperson = document.createElement('div');
+       let  reservationhalf = document.createElement('div');
+       let  reservationfull = document.createElement('div');
+       let  walkinhalf = document.createElement('div');
+       let  walkinfull = document.createElement('div');
+       h2.textContent = data[i].rentalname;
+       maxperson.setAttribute('class', 'maxperson')
+       maxperson.textContent = 'Max Person(s); ' + data[i].maxperson;
+       reservationhalf.setAttribute('class', 'reservationhalf')
+       reservationhalf.textContent = 'Reservation-Half Day (3 hrs): ' + data[i].reservationhalf;
+       reservationfull.setAttribute('class', 'reservationfull')
+       reservationfull.textContent = 'Reservation-Full Day: ' + data[i].reservationfull;
+       walkinhalf.setAttribute('class', 'walkinhalf')
+       walkinhalf.textContent = 'Walk In-Half Day (3 hrs): ' + data[i].walkinhalf;
+       walkinfull.setAttribute('class', 'walkinfull')
+       walkinfull.textContent = 'Walk In- Full Day: ' + data[i].walkinfull;
+       image.setAttribute('src', data[i].imageurl);
+       image.setAttribute('alt', data[i].rentalname);
+       card.appendChild(h2);
+       card.appendChild(maxperson);
+       card.appendChild(reservationhalf);
+       card.appendChild(reservationfull);
+       card.appendChild(walkinhalf);
+       card.appendChild(walkinfull);
+       card.appendChild(image);
+       document.querySelector('div.cards').appendChild(card);
+   }
+});
---
+++ final/js/weather.js
@@ -0,0 +1,43 @@
+const apiURL_weather= 'https://api.openweathermap.org/data/2.5/weather?id=3530103&appid=2c6bdbd53e64e351529171a4fe9b31ed&units=imperial';
+    fetch(apiURL_weather)    
+    .then(function (response) {
+    return response.json();
+})
+    .then(function (jsonObject) {
+        let temp = jsonObject.main.temp;
+        let speed = jsonObject.wind.speed;
+
+        document.getElementById('currently').textContent = jsonObject.weather[0].description;
+        document.getElementById('temp').textContent = Math.round(temp);
+        document.getElementById('humidity').textContent = jsonObject.main.humidity;
+        document.getElementById('speed').textContent = Math.round(speed);
+
+        let output = "N/A";
+        if (temp <= 50 && speed > 3) {
+            output = Math.round(35.74 + 0.6215 * temp - 35.75 * speed ** 0.16 + 0.4275 * temp * speed ** 0.16);
+            output += " \xB0F";
+        }
+        document.getElementById("windchill").textContent = output;
+  });
+
+const apiURL_forecast= 'https://api.openweathermap.org/data/2.5/forecast?id=3530103&appid=2c6bdbd53e64e351529171a4fe9b31ed&units=imperial';
+    fetch(apiURL_forecast)
+    .then(response => response.json())
+    .then((jsObject) => {
+    console.log(jsObject);
+    const forecastData = jsObject.list.filter((element)=>element.dt_txt.includes('18:00:00'));
+
+    console.log(forecastData);
+
+    const weekdays = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+
+   let day = 0;
+  forecastData.forEach(forecast => {
+    let x = new Date(forecast.dt_txt);
+   document.getElementById('temp'+(day+1)).textContent = Math.round(forecast.main.temp) + ' °F';
+   document.getElementById('img'+(day+1)).src = "https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
+   document.getElementById('img'+(day+1)).alt = forecast.weather[0].description;
+   document.getElementById('day'+(day+1)).textContent = weekdays[x.getDay()];
+   day++;	  
+  });
+});
